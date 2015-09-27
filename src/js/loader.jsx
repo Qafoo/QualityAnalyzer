dab3a26b03464732b5b0549dd5e44daa6a1bc4dc
Kore Nordmann <mail@kore-nordmann.de>
2015-09-27 12:19:12 +0200

    Load project files

--- src/js/loader.jsx
+++ src/js/loader.jsx
@@ -7,6 +7,33 @@ var app = app || {};
     };
 
     app.Loader = React.createClass({
+        componentDidMount: function() {
+            $.getJSON("data/project.json", null, function(data) {
+                app.Data = data;
+
+                var defaults = {
+                        type: "GET",
+                        cache: false
+                    },
+                    deferreds = $.map(app.Data.analyzers, function(file, analyzer) {
+                        return $.ajax(
+                            $.extend({
+                                    url: "data/" + file,
+                                    success: function(data) {
+                                        app.Data.analyzers[analyzer] = xml.xmlToJSON(data);
+                                    }
+                                },
+                                defaults
+                            )
+                        );
+                    });
+
+                $.when.apply($, deferreds).then(function() {
+                    console.log("All done");
+                    console.log(app.Data);
+                });
+            });
+        },
         render: function() {
             return (<div className="row">
                 <div className="col-md-6 col-md-offset-3">
