1090805dd529af68d164c67a3e4bc10f589463c0
Kore Nordmann <mail@kore-nordmann.de>
2015-09-27 10:18:34 +0200

    Improved loading screen

--- src/js/loader.jsx
+++ src/js/loader.jsx
@@ -3,12 +3,22 @@ var app = app || {};
 (function () {
     "use strict";
 
+    app.Data = {
+    };
+
     app.Loader = React.createClass({
         render: function() {
             return (<div className="row">
                 <div className="col-md-6 col-md-offset-3">
                     <div className="well">
-                        <p>Loading data…</p>
+                        <p>
+                            <span className="glyphicon glyphicon-ok"></span>
+                            Initializing application
+                        </p>
+                        <p>
+                            <span className="glyphicon glyphicon-hourglass"></span>
+                            Loading data
+                        </p>
                     </div>
                 </div>
             </div>);
