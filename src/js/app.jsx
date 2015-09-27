02aa3315b6eec199ddd20fd48e3152bbd4fe6840
Kore Nordmann <mail@kore-nordmann.de>
2015-09-27 10:18:14 +0200

    Ensure application components are there…

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -3,7 +3,12 @@ var app = app || {};
 (function () {
     "use strict";
 
+    var Data = app.Data;
     var Loader = app.Loader;
+
+    if (!(app.Data && app.Loader)) {
+        throw "Some application components were not correctly loaded.";
+    }
 
     React.render(
         <Loader/>,
