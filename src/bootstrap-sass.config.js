09a0ba91d5272d726338570dacb085ad489149b2
Kore Nordmann <mail@kore-nordmann.de>
2015-10-12 09:23:58 +0200

    Build all required bootstrap components

--- src/bootstrap-sass.config.js
+++ src/bootstrap-sass.config.js
@@ -7,15 +7,69 @@ module.exports = {
     debug: false,
     styleLoader: ExtractTextPlugin.extract("style-loader", "css-loader!sass?outputStyle=expanded"),
     scripts: {
-        'transition': true
+        "affix": false,
+        "alert": false,
+        "button": false,
+        "carousel": false,
+        "collapse": false,
+        "dropdown": false,
+        "modal": false,
+        "popover": false,
+        "scrollspy": false,
+        "slim_navigation": false,
+        "tab": false,
+        "tags": false,
+        "tooltip": false,
+        "transition": true
     },
     styles: {
         "mixins": true,
- 
+
+        // Reset and dependencies
         "normalize": true,
         "print": true,
- 
+        "glyphicons": false,
+
+        // Core CSS
         "scaffolding": true,
-        "type": true
+        "type": true,
+        "code": true,
+        "grid": true,
+        "tables": true,
+        "forms": true,
+        "buttons": true,
+
+        // Components
+        "component-animations": true,
+        "dropdowns": true,
+        "button-groups": true,
+        "input-groups": true,
+        "navs": true,
+        "navbar": true,
+        "breadcrumbs": true,
+        "pagination": true,
+        "pager": true,
+        "labels": true,
+        "badges": true,
+        "jumbotron": false,
+        "thumbnails": false,
+        "alerts": false,
+        "progress-bars": true,
+        "media": false,
+        "list-group": true,
+        "panels": true,
+        "responsive-embed": true,
+        "wells": false,
+        "close": true,
+
+        // Components w/ JavaScript
+        "modals": false,
+        "tooltip": false,
+        "popovers": false,
+        "carousel": false,
+
+        // Utility classes
+        "utilities": true,
+        "responsive-utilities": true,
     }
 };
