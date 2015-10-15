b79c312bfa31c2100403473d0925441dfa1d67b0
Kore Nordmann <mail@kore-nordmann.de>
2015-10-15 10:25:02 +0200

    Defined command options

--- src/php/Qafoo/Analyzer/Command/Analyze.php
+++ src/php/Qafoo/Analyzer/Command/Analyze.php
@@ -19,18 +19,52 @@ class Analyze extends Command
                 'path',
                 InputArgument::REQUIRED,
                 'Path to the source code which should be analyzed'
-            )
-            ->addOption(
-               'yell',
+            )->addOption(
+               'coverage',
+               'c',
+               InputOption::VALUE_REQUIRED,
+               'Path to code coverage (clover) XML file'
+            )->addOption(
+               'pdepend',
                null,
-               InputOption::VALUE_NONE,
-               'If set, the task will yell in uppercase letters'
-            )
-        ;
+               InputOption::VALUE_REQUIRED,
+               'Path to PDepend summary XML file'
+            )->addOption(
+               'dependencies',
+               null,
+               InputOption::VALUE_REQUIRED,
+               'Path to PDepend dependencies XML file'
+            )->addOption(
+               'phpmd',
+               null,
+               InputOption::VALUE_REQUIRED,
+               'Path to mess detector (PMD / PHPMD) XML file'
+            )->addOption(
+               'checkstyle',
+               null,
+               InputOption::VALUE_REQUIRED,
+               'Path to checkstyle violations (PHP Code Sniffer) XML file'
+            )->addOption(
+               'tests',
+               't',
+               InputOption::VALUE_REQUIRED,
+               'Path to jUnit (PHPUnit) test result XML file'
+            )->addOption(
+               'cpd',
+               null,
+               InputOption::VALUE_REQUIRED,
+               'Path to C&P violations (PHP Copy Paste Detector) XML file'
+            )->addOption(
+               'exclude',
+               'x',
+               InputOption::VALUE_REQUIRED,
+               'Directories to exclude from analyzing'
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        var_dump($input->getOption('exclude'));
         $output->writeln('Analyze…');
     }
 }
