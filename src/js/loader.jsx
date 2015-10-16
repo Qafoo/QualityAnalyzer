3f4e09d2642ee300adea52215eddfb5df9da7e32
Kore Nordmann <mail@kore-nordmann.de>
2015-10-16 14:31:16 +0200

    Expose loading errors

--- src/js/loader.jsx
+++ src/js/loader.jsx
@@ -8,6 +8,7 @@ import xml2js from 'xml2js';
 let Loader = React.createClass({
     getInitialState: function() {
         return {
+            error: null,
             done: false,
             progress: 0
         };
@@ -18,52 +19,67 @@ let Loader = React.createClass({
             progress: this.state.progress + step
         });
     },
+    
+    loadProjectData: function(projectData) {
+        var data = projectData,
+            component = this,
+            defaults = {
+                type: "GET",
+                cache: false,
+                dataType: "text"
+            },
+            step = 100 / Object.keys(data.analyzers).length,
+            deferreds = jQuery.map(data.analyzers, function(file, analyzer) {
+                return jQuery.ajax(
+                    jQuery.extend({
+                            url: "/data/" + file,
+                        },
+                        defaults
+                    )
+                ).pipe(function(analyzerData) {
+                    var deferred = jQuery.Deferred(),
+                        parser = new xml2js.Parser();
 
-    componentDidMount: function() {
-        var component = this,
-            data = {};
-
-        jQuery.getJSON("/data/project.json", null, function(projectData) {
-            data = projectData;
-
-            var defaults = {
-                    type: "GET",
-                    cache: false,
-                    dataType: "text"
-                },
-                step = 100 / Object.keys(data.analyzers).length,
-                deferreds = jQuery.map(data.analyzers, function(file, analyzer) {
-                    return jQuery.ajax(
-                        jQuery.extend({
-                                url: "/data/" + file,
-                            },
-                            defaults
-                        )
-                    ).pipe(function(analyzerData) {
-                        var deferred = jQuery.Deferred(),
-                            parser = new xml2js.Parser();
-
-                        parser.parseString(analyzerData, function(error, result) {
-                            if (error) {
-                                deferred.reject(error);
-                            }
-                            deferred.resolve(result);
-                        });
-
-                        return deferred.promise();
-                    }).pipe(function(result) {
-                        data.analyzers[analyzer] = result;
-                        component.advanceProgress(step);
-                        return data;
+                    parser.parseString(analyzerData, function(error, result) {
+                        if (error) {
+                            deferred.reject(error);
+                        }
+                        deferred.resolve(result);
                     });
-                });
 
-            jQuery.when.apply(jQuery, deferreds).then(function() {
+                    return deferred.promise();
+                }).pipe(function(result) {
+                    data.analyzers[analyzer] = result;
+                    component.advanceProgress(step);
+                    return data;
+                });
+            });
+
+        jQuery.when.apply(jQuery, deferreds)
+            .done(function() {
                 component.setState({done: true, progress: 100});
                 if (component.props.onComplete) {
                     component.props.onComplete(data);
                 }
+            }).fail(function(error) {
+                component.setState({
+                    error: "Failed loading file: " + error
+                });
             });
+    },
+
+    componentDidMount: function() {
+        var component = this;
+
+        jQuery.ajax({
+            dataType: "json",
+            url: "/data/project.json",
+            success: this.loadProjectData,
+            error: function(jqXHR, textStatus, errorThrown) {
+                component.setState({
+                    error: "Could not find project.json. Run `bin/analyze analyze path/t/source` and reload."
+                });
+            }
         });
     },
 
@@ -83,6 +99,10 @@ let Loader = React.createClass({
                         Loading data
                     </p>
                     <ProgressBar progress={this.state.progress} />
+                    { !this.state.error ? '' :
+                    <div className="alert alert-danger">
+                        {this.state.error}
+                    </div>}
                 </div>
             </div>
         </div>);
