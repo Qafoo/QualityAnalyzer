a056fa8755626a32ec8366b2ae1516eff9bad34d
Kore Nordmann <mail@kore-nordmann.de>
2015-10-21 11:01:15 +0200

    Show coverage information is source browser

--- src/js/source/file.jsx
+++ src/js/source/file.jsx
@@ -6,9 +6,12 @@ let SourceFile = React.createClass({
         var file = this.props.file,
             nodeSelected = file.name === this.props.selected[0];
 
+        console.log(file);
+
         return (<li className={nodeSelected ? "selected" : ""}>
             <Router.Link to={"/source/" + file.file.name}>
-                <span className="glyphicon glyphicon-file"></span> <span className="name">{this.props.file.name}</span>
+                <span className={"glyphicon glyphicon-file " + (file.coverage > .8 ? "covered" : (file.coverage > .5 ? "semi-covered" : "uncovered"))} ></span>&nbsp;
+                <span className="name">{this.props.file.name}</span>
             </Router.Link>
         </li>);
     }
