519d68b3dc6af828a4405a72957b1c5be6dee113
Kore Nordmann <mail@kore-nordmann.de>
2015-12-03 08:41:27 +0100

    Removed broken include

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,7 +54,6 @@ module.exports = function(grunt) {
     });
  
     grunt.loadNpmTasks('grunt-karma');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-webpack');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-eslint');
