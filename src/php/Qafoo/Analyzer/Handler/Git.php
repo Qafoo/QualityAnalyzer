2bf9b0ea40fc991c8133d66c007e9b85cb8ee0f0
Kore Nordmann <mail@kore-nordmann.de>
2016-08-20 10:05:30 +0200

    Use better file type

--- src/php/Qafoo/Analyzer/Handler/Git.php
+++ src/php/Qafoo/Analyzer/Handler/Git.php
@@ -34,7 +34,7 @@ class Git extends Handler
      */
     public function handle(Project $project, $existingResult = null)
     {
-        $resultFile = $this->shell->getTempFile() . '.js';
+        $resultFile = $this->shell->getTempFile() . '.json';
         $result = array(
             '3' => $this->countGitChangesPerFile($project, new \DateTime('-3 months')),
             '12' => $this->countGitChangesPerFile($project, new \DateTime('-1 year')),
