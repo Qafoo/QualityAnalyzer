3ac361cfb83aac4a666f0b44a383ac6ef305ae5c
Tobias Schlitt <toby@php.net>
2016-12-14 10:28:38 +0100

    Fixed: Content type for JSON.

--- bin/serve.php
+++ bin/serve.php
@@ -2,7 +2,14 @@
 
 $baseDir = __DIR__ . '/../';
 if (file_exists($baseDir . $_SERVER['SCRIPT_NAME'])) {
-    return false;
+    // Workaround for PHP web server not recognizing JSON mime type;
+    if (substr($_SERVER['SCRIPT_NAME'], -4, 4) === 'json') {
+        header('Content-Type: application/json');
+        readfile($baseDir . $_SERVER['SCRIPT_NAME']);
+        exit(0);
+    } else {
+        return false;
+    }
 }
 
 require $baseDir . 'index.html';
